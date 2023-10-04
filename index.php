148128c52985d03066ed60493827dbe191d0ef55
Vincnet <ketylove83@gmail.com>
2023-10-04 16:46:21 +0300

    Second Commit

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-echo '!!!!Welcome PHP!!!';
+$arr = [1,3,4,6,7,8];
 
+foreach ($arr as $value) {
+    echo $value.'<br>';
+}
+
+echo '!!!!Welcome PHP!!!';
 ?>
 
 <h1>Привет</h1>
+<br>
+<h2>!!!!!</h2>>
