d6935b713f28f4e9179662e3ff0c46629d3d7307
kdvbpspr <kdvbpspr1@gmail.com>
2023-10-12 16:35:25 +0300

    #start

--- public/index.php
+++ public/index.php
@@ -2,4 +2,17 @@
 
 declare(strict_types=1);
 
-phpinfo();
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello Slim!');
+    return $response->withHeader('Content-Type', 'text/plain');
+});
+
+$app->run();
