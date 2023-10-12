8d3dbb02e6b67dc63607cab7a389a4a8ee3bbd41
kdvbpspr <kdvbpspr1@gmail.com>
2023-10-12 16:39:13 +0300

    #start

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
-use Slim\Psr7\Request;
-use Slim\Psr7\Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
