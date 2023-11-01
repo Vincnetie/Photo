f0eb81d9b6d5b244dbbacca808cfc9300706290a
kdvbpspr <kdvbpspr1@gmail.com>
2023-11-01 15:36:47 +0300

    #Add upload

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -9786,35 +9786,37 @@ __webpack_require__.r(__webpack_exports__);
 
 document.addEventListener('DOMContentLoaded', function () {
   // Проверяем текущий маршрут
-  var myDropzone = new dropzone__WEBPACK_IMPORTED_MODULE_0__["default"]("#myDropzone", {
-    url: "/upload",
-    // Замените на URL, куда вы хотите отправлять загруженные файлы
-    autoProcessQueue: false,
-    // Отключение автоматической загрузки файлов
-    maxFilesize: 2,
-    // Максимальный размер файла в мегабайтах
-    acceptedFiles: ".jpg,.png,.gif",
-    // Принимаемые типы файлов
-    dictDefaultMessage: "Перетащите сюда файлы для загрузки",
-    addRemoveLinks: true // Отображение ссылок для удаления загруженных файлов
-  });
+  if (window.location.pathname === '/upload') {
+    var myDropzone = new dropzone__WEBPACK_IMPORTED_MODULE_0__["default"]("#myDropzone", {
+      url: "/upload",
+      // Замените на URL, куда вы хотите отправлять загруженные файлы
+      autoProcessQueue: false,
+      // Отключение автоматической загрузки файлов
+      maxFilesize: 2,
+      // Максимальный размер файла в мегабайтах
+      acceptedFiles: ".jpg,.png,.gif",
+      // Принимаемые типы файлов
+      dictDefaultMessage: "Перетащите сюда файлы для загрузки",
+      addRemoveLinks: true // Отображение ссылок для удаления загруженных файлов
+    });
 
-  // Обработка успешной загрузки файла
-  myDropzone.on("success", function (file, response) {
-    console.log("Файл успешно загружен:", response);
-  });
+    // // Обработка успешной загрузки файла
+    // myDropzone.on("success", function(file, response) {
+    //     console.log("Файл успешно загружен:", response);
+    // });
+    //
+    // // Обработка удаления файла
+    // myDropzone.on("removedfile", function(file) {
+    //     console.log("Файл удален:", file);
+    // });
 
-  // Обработка удаления файла
-  myDropzone.on("removedfile", function (file) {
-    console.log("Файл удален:", file);
-  });
-
-  // Обработка события нажатия на кнопку отправки формы
-  document.querySelector("#submitBtn").addEventListener("click", function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    myDropzone.processQueue(); // Запуск процесса загрузки файлов
-  });
+    // Обработка события нажатия на кнопку отправки формы
+    document.querySelector("#submitBtn").addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      myDropzone.processQueue(); // Запуск процесса загрузки файлов
+    });
+  }
 });
 
 __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,7 @@ import Dropzone from 'dropzone';
 
 document.addEventListener('DOMContentLoaded', function() {
     // Проверяем текущий маршрут
+    if (window.location.pathname === '/upload') {
         var myDropzone = new Dropzone("#myDropzone", {
             url: "/upload", // Замените на URL, куда вы хотите отправлять загруженные файлы
             autoProcessQueue: false, // Отключение автоматической загрузки файлов
@@ -11,15 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
             addRemoveLinks: true // Отображение ссылок для удаления загруженных файлов
         });
 
-        // Обработка успешной загрузки файла
-        myDropzone.on("success", function(file, response) {
-            console.log("Файл успешно загружен:", response);
-        });
-
-        // Обработка удаления файла
-        myDropzone.on("removedfile", function(file) {
-            console.log("Файл удален:", file);
-        });
+        // // Обработка успешной загрузки файла
+        // myDropzone.on("success", function(file, response) {
+        //     console.log("Файл успешно загружен:", response);
+        // });
+        //
+        // // Обработка удаления файла
+        // myDropzone.on("removedfile", function(file) {
+        //     console.log("Файл удален:", file);
+        // });
 
         // Обработка события нажатия на кнопку отправки формы
         document.querySelector("#submitBtn").addEventListener("click", function(e) {
@@ -27,6 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
             e.stopPropagation();
             myDropzone.processQueue(); // Запуск процесса загрузки файлов
         });
+    }
 });
 
 require('bootstrap');
