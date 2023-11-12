2ac17fca49b1232d580c8a7d98062de55e547aa3
kdvbpspr <kdvbpspr1@gmail.com>
2023-11-12 16:01:50 +0300

    #Add edit photo

---
+++ src/Http/Action/DeleteAction.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Action;
+
+use PDO;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class DeleteAction
+{
+    private Twig $twig;
+    private PDO $pdo;
+
+    public function __construct(Twig $twig, PDO $pdo)
+    {
+        $this->twig = $twig;
+        $this->pdo = $pdo;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        // Получаем значение параметра id из URL
+        $id = $args['id'];
+
+        // Проверяем, есть ли фотография с заданным id
+        $statement = $this->pdo->prepare('SELECT * FROM photos WHERE id = :id');
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+        $photo = $statement->fetch();
+
+        // Если фотография не найдена, выводим предупреждение
+        if (!$photo) {
+            return $this->twig->render($response, 'error.twig', [
+                'message' => 'Фотография не найдена'
+            ])->withStatus(404);
+        }
+
+        // Выполняем операцию удаления фотографии из базы данных
+        $statement = $this->pdo->prepare('DELETE FROM photos WHERE id = :id');
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+
+        // Перенаправляем пользователя на страницу со списком фотографий
+        return $response->withHeader('Location', '/photos')->withStatus(302);
+    }
+}
