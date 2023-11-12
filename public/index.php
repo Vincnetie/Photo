018bae6f42f6f5c262672c66914e643c5c05d520
kdvbpspr <kdvbpspr1@gmail.com>
2023-11-12 11:36:01 +0300

    #Add MySQL

--- public/index.php
+++ public/index.php
@@ -23,6 +23,22 @@ $builder->addDefinitions([
     'config' => [
         'debug' => true,
     ],
+    PDO::class => function (ContainerInterface $container) {
+        $host = 'mysql';
+        $dbname = 'photo';
+        $username = 'app';
+        $password = 'secret';
+
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+        $options = [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        return new PDO($dsn, $username, $password, $options);
+    },
     Twig::class => function (ContainerInterface $container) {
         $twig = Twig::create(__DIR__ . '/../templates', [
             'cache' => false,
--- src/Http/Action/UploadAction.php
+++ src/Http/Action/UploadAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Action;
 
+use PDO;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -10,14 +11,32 @@ use Slim\Http\UploadedFile;
 class UploadAction
 {
     private Twig $twig;
+    private PDO $pdo;
 
-    public function __construct(Twig $twig)
+    public function __construct(Twig $twig, PDO $pdo)
     {
         $this->twig = $twig;
+        $this->pdo = $pdo;
+    }
+
+    private function createPhotosTable(): void
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS photos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+
+        $this->pdo->exec($sql);
     }
 
     public function __invoke(Request $request, Response $response): Response
     {
+
+        // Создание таблицы photos, если она отсутствует
+        $this->createPhotosTable();
+
         if ($request->getMethod() === 'GET') {
 
             $data = [
@@ -26,6 +45,10 @@ class UploadAction
 
             return $this->twig->render($response, 'upload.twig', $data);
         }
+
+        // Выполнение запроса к базе данных
+        //$statement = $this->pdo->query('SELECT * FROM mytable');
+        //$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
         $uploadedFiles = $request->getUploadedFiles();
 
